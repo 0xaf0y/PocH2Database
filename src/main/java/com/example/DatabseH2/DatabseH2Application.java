d6652f045500ad54af736f57ec598866b05ee8eb
Lucas Costa <0xaf0y@gmail.com>
2020-01-26 16:58:27 -0300

    commit inicial

---
+++ src/main/java/com/example/DatabseH2/DatabseH2Application.java
@@ -0,0 +1,13 @@
+package com.example.DatabseH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabseH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabseH2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DatabseH2/DatabseH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DatabseH2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabseH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
